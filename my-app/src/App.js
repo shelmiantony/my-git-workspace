2974d161ed998a633fc1245cd44279d387bfdf47
Shelmi <shelmika23@gmail.com>
2023-10-27 16:49:11 +0530

    added sample omponent

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Sample from './Component/Sample'
+function App() {
+  return (
+    <div className="App">
+  <Sample/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Component/Sample.js
@@ -0,0 +1,13 @@
+
+
+
+
+function Sample() {
+  return (
+    <div>
+ Helooooooooooooooooooooooo workspace................!!!!!!!!!!!!!!!!!!!!!!!11
+    </div>
+  );
+}
+
+export default  Sample;
