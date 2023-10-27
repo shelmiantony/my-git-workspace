254f6e200ae4083fa90779b14d78bfaa0af5aedd
Shelmi <shelmika23@gmail.com>
2023-10-27 20:43:54 +0530

    modified omponent

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 
 
 // import Sample from './Component/Sample'
-import Square from './Component/Test'
+import Board from './Component/Board'
 // import About from './Component/About Page/About'
 function App() {
   return (
     <div className="App">
        <h1>Welcome to my app</h1>
-        <Square/>
+        <Board/>
   {/* <Sample/> */}
   {/* <About/>  */}
     </div>
