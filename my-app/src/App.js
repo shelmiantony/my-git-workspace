791f2287887a271f36702a874725f553e9f470ac
shelmiantony <79745830+shelmiantony@users.noreply.github.com>
2023-10-27 20:18:47 +0530

    Added more than one buttons for tic-tac-toe game (#4)
    * Test file modified with Button
    
    * added more than one square in test file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 
 
-import Sample from './Component/Sample'
-import Test from './Component/Test'
+// import Sample from './Component/Sample'
+import Square from './Component/Test'
+// import About from './Component/About Page/About'
 function App() {
   return (
     <div className="App">
-        <Test/>
+       <h1>Welcome to my app</h1>
+        <Square/>
   {/* <Sample/> */}
+  {/* <About/>  */}
     </div>
   );
 }
