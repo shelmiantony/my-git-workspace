bbaf0ff9e2d72994fdaac6cc5a1e3cb1c687ce47
Shelmi <shelmika23@gmail.com>
2024-01-01 17:30:50 +0530

    Final code

--- my-app/src/Component/Game.js
+++ my-app/src/Component/Game.js
@@ -2,24 +2,43 @@ import React, { useState } from 'react';
 import Board from './Board'
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
+  
   const [history, setHistory] = useState([Array(9).fill(null)]);
-
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   // Get the current state, or an empty array if history is empty
-  const currentSquares = history.length > 0 ? history[history.length - 1] : Array(9).fill(null);
-
+  // const currentSquares = history.length > 0 ? history[history.length - 1] : Array(9).fill(null);
+  const currentSquares = history[currentMove];
   function handlePlay(nextSquares) {
-    setHistory((prevHistory) => [...prevHistory, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    // setXIsNext(!xIsNext);
   }
-
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    // setXIsNext(nextMove % 2 === 0);
+  }
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
