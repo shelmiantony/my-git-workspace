ab707c16b64a23917231301172d2e30ef79df290
Shelmi <shelmika23@gmail.com>
2024-02-02 16:43:12 +0530

    Added route file

---
+++ my-app/src/Component/Home/HomePage.js
@@ -0,0 +1,14 @@
+
+
+
+function Home() {
+  return (
+    <div className="App">
+      
+      <h1>Home Page</h1> 
+       
+    </div>
+  );
+}
+
+export default Home;
---
+++ my-app/src/routes.js
@@ -0,0 +1,32 @@
+
+
+
+import '../src/App.css'
+
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+ import Game from './Component/TicTacToe/Game'
+function RouterFun(){
+ return (
+  <BrowserRouter>
+    <div className="App">
+      <ul>
+       <li>
+         <Link to="/tictactoe">TicTacToe</Link>
+       </li>
+       {/* <li>
+         <Link to="/about">About</Link>
+       </li>
+       <li>
+         <Link to="/contact">Contact</Link>
+       </li> */}
+     </ul>
+    <Routes>
+       <Route exact path="/tictactoe" element={<Game />}></Route>
+       {/* <Route exact path="/about" element={<About />}></Route>
+       <Route exact path="/contact" element={<Contact />}></Route>             */}
+    </Routes>
+   </div>
+  </BrowserRouter>
+ );
+}
+export default App;
