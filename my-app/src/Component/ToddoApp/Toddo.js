9b71a501f893b62b0cf52a46815e30961b7c744d
shelmiantony <79745830+shelmiantony@users.noreply.github.com>
2024-07-09 18:38:51 +0530

    Added complete code for toddo app (#13)
    * Test file modified with Button
    
    * added more than one square in test file
    
    * modified omponent
    
    * Deleted about page
    
    * modified app.js page
    
    * props passed from parent to hild
    
    * Added latest code
    
    * Winner Code
    
    * Final code
    
    * solved conflict
    
    * For the current move only, show You are at move” instead of a button.
    
    * changed the style of sqaure
    
    * Avoiding hardcoding of Sqaures
    
    * Move to new folder
    
    * Added route file
    
    * Added new website app
    
    * latest code
    
    * Remove toddoapp folder from repository
    
    * added reducer into app
    
    * latest code
    
    * latest code
    
    * latest code
    
    * latest code

--- my-app/src/Component/ToddoApp/Toddo.js
+++ my-app/src/Component/ToddoApp/Toddo.js
@@ -1,34 +1,73 @@
+// Todo.js
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import TodoList from './TodoList';
+import FilterButtons from './FilterCom';
+import { BsSearch, BsPlus } from 'react-icons/bs';
+import { addTodo, updateSearchTerm } from '../../Redux/action';
 
-import React, { useState } from 'react'
-import {useDispatch} from 'react-redux'
-import { BsPlus } from "react-icons/bs";
-import {addTodo}  from '../../Redux/action'
+const Todo = () => {
+  const todos = useSelector((state) => state.todos);
+  const filter = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
+  const [newTodoText, setNewTodoText] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
-function Todo (){
-    const [todos,setTodos] = useState('')
-    const dispatch = useDispatch();
-    console.log("todos............>",todos)
-    const handleChange = (e) =>{
-setTodos(e.target.value)
+  const handleAddTodo = (text) => {
+    dispatch(addTodo(text));
+  };
+
+  const handleAddTodoClick = () => {
+    if (newTodoText.trim() !== '') {
+      handleAddTodo(newTodoText.trim());
+      setNewTodoText('');
     }
-    const handleAdd = () => {
-        if(todos.trim()){
-dispatch(addTodo(todos))
-        }
-    }
-    return(
-        <div className='max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded'>
+  };
 
-            <h2 className='mt-3 mb-6 text-3xl font-bold text-center uppercase'>PERSONAL TODO APP</h2>
-            <div className='flex items-center mb-4'>
-                <input value={todos} onChange={handleChange} type='text' name='addTodoInput' id='addTodoInput'placeholder='Add Todo'
-                className='flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-300 '/>
-                <button onClick={handleAdd}
-                 className='ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none'><BsPlus /></button>
-            </div>
+  const handleSearchChange = (value) => {
+    setSearchTerm(value);
+    dispatch(updateSearchTerm(value));
+  };
+
+  return (
+    <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
+      <h2 className='mt-3 mb-6 text-2xl font-bold text-center uppercase'>Personal TODO APP</h2>
+      <div className="flex items-center mb-4">
+        <input
+          id="addTodoInput"
+          className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
+          type="text"
+          placeholder="Add Todo"
+          value={newTodoText}
+          onChange={(e) => setNewTodoText(e.target.value)}
+        />
+        <button
+          className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+          onClick={handleAddTodoClick}
+        >
+          <BsPlus size={20} />
+        </button>
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
+        <FilterButtons />
+        <div className="flex items-center mb-4">
+          <input
+            className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
+            type="text"
+            placeholder="Search Todos"
+            value={searchTerm}
+            onChange={(e) => handleSearchChange(e.target.value)}
+          />
+          <button className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">
+            <BsSearch size={20} />
+          </button>
         </div>
-    )
-}
+      </div>
+
+      <TodoList />
+    </div>
+  );
+};
+
 export default Todo;
-
-
