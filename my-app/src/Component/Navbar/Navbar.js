c2788a2f6893108ef636d36a9e56a081a317e8e1
shelmiantony <79745830+shelmiantony@users.noreply.github.com>
2024-07-14 17:19:47 +0530

    Latest code in navbar (#16)
    * Test file modified with Button
    
    * added more than one square in test file
    
    * modified omponent
    
    * Deleted about page
    
    * modified app.js page
    
    * props passed from parent to hild
    
    * Added latest code
    
    * Winner Code
    
    * Final code
    
    * solved conflict
    
    * For the current move only, show You are at move” instead of a button.
    
    * changed the style of sqaure
    
    * Avoiding hardcoding of Sqaures
    
    * Move to new folder
    
    * Added route file
    
    * Added new website app
    
    * latest code
    
    * Remove toddoapp folder from repository
    
    * added reducer into app
    
    * latest code
    
    * latest code
    
    * latest code
    
    * latest code
    
    * Added more components
    
    * Added about page
    
    * latest code with about page
    
    * Latest aboutjs page
    
    * Added navbar
    
    * latest navbar

--- my-app/src/Component/Navbar/Navbar.js
+++ my-app/src/Component/Navbar/Navbar.js
@@ -1,13 +1,28 @@
-import { AppBar ,Toolbar,Typography} from '@mui/material'
+import { AppBar ,Toolbar,Typography,Button} from '@mui/material'
+import { Link } from 'react-router-dom';
 import React from 'react'
-
+import Projects from '../Projects/Project';
+import About from '../About/About';
 function Navbar() {
   return (
-    <AppBar position="static" className="bg-gray-900">
+    <AppBar position="static"sx={{ backgroundColor: '#4CAF50' }}>
     <Toolbar>
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
         My Portfolio
       </Typography>
+      <Button component={Link} to="/About" color="inherit">
+      Resume
+        </Button>
+        <Button component={Link} to="/Projects" color="inherit">
+          Projects
+        </Button>
+        <Button component={Link} to="/Projects" color="inherit">
+          Resume
+        </Button>
+        <Button component={Link} to="/Projects" color="inherit">
+         Contact
+        </Button>
+        
       </Toolbar>
       </AppBar>
   )
