2f6f48fe1de6da3404db030ef9071443e1a34634
Shelmi <shelmika23@gmail.com>
2024-07-14 16:07:47 +0530

    latest code with about page

--- my-app/src/Component/About/About copy.js
+++ my-app/src/Component/About/About copy.js
@@ -1,44 +1,60 @@
-import React from "react";
-import { Button, Typography, Container, Box } from '@mui/material';
+import React from 'react';
+import { Container, Typography, Box, Button, Card, CardContent, Avatar, IconButton, Grid, Paper } from '@mui/material';
+import { LinkedIn, GitHub, Twitter } from '@mui/icons-material';
+import Profile from '../../Assets/Profile.jpg';
 
-export default function About() {
+function About() {
   return (
-    <section id="about" className="bg-gray-900 text-white py-20">
-      <Container className="flex flex-col md:flex-row items-center">
-        <Box className="flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <Typography variant="h3" component="h1" className="mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing apps.
-          </Typography>
-          <Typography variant="body1" component="p" className="mb-8 leading-relaxed text-gray-400">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui laborum quasi, incidunt dolore iste nostrum cupiditate voluptas? Laborum, voluptas natus?
-          </Typography>
-          <Box className="flex justify-center space-x-4">
-            <Button
-              href="#contact"
-              variant="contained"
-              color="primary"
-              className="mr-4"
-            >
-              Work With Me
-            </Button>
-            <Button
-              href="#projects"
-              variant="contained"
-              color="secondary"
-            >
-              See My Past Work
-            </Button>
-          </Box>
-        </Box>
-        <Box className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
-          />
-        </Box>
+    <section id='about' className='text-gray-900 py-20' style={{ backgroundColor: '#F4ECE6' }}>
+      <Container>
+        <Paper style={{ padding: '20px', backgroundColor: '#F4ECE6' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <Box className='flex flex-col items-center'>
+                <Card className='bg-gray-800'>
+                  <CardContent className='flex flex-col items-center'>
+                    <Avatar src={Profile} alt='Profile' className='w-32 h-32 mb-4 rounded-full' />
+                    <Typography variant='h4' component='h1' className='text-white mb-2'>
+                      Hi, I'm Shelmi
+                    </Typography>
+                    <Box className='flex justify-center space-x-4 mb-4'>
+                      <IconButton href='https://www.linkedin.com' target='_blank' className='text-white'>
+                        <LinkedIn />
+                      </IconButton>
+                      <IconButton href='https://github.com' target='_blank' className='text-white'>
+                        <GitHub />
+                      </IconButton>
+                      <IconButton href='https://twitter.com' target='_blank' className='text-white'>
+                        <Twitter />
+                      </IconButton>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Box className='flex-grow flex flex-col items-center md:items-start text-center md:text-left'>
+                <Typography variant='h3' component='h1' className='mb-4 font-medium text-gray-900'>
+                  About Me
+                </Typography>
+                <Typography variant='body1' component='p' className='mb-8 leading-relaxed text-gray-600'>
+                  To achieve a challenging position in a professional organization through self-improvement by excelling in all responsibilities with sincere hard work, dedication & commitment. To work towards the development of the organization & grow with it.
+                </Typography>
+                <Box className='flex justify-center md:justify-start space-x-4'>
+                  <Button href='#resume' variant='contained' color='primary'>
+                    Resume
+                  </Button>
+                  <Button href='#projects' variant='contained' color='secondary'>
+                    See My Projects
+                  </Button>
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </Paper>
       </Container>
     </section>
   );
 }
+
+export default About;
--- my-app/src/Component/About/About.js
+++ my-app/src/Component/About/About.js
@@ -1,36 +1,64 @@
-import { Container, Typography, Box, Button } from '@mui/material';
-import Profile from '../../Assets/Profile.jpg';
-// ./src/Assets/Profile.jp
 import React from 'react';
+import { Container, Typography, Box, Button, Card, CardContent, Avatar, IconButton ,Grid,Paper} from '@mui/material';
+import { LinkedIn, GitHub, Twitter } from '@mui/icons-material';
+import Profile from '../../Assets/Profile.jpg';
 
 function About() {
   return (
-    <section id='about' className='text-white bg-gray-900 py-20'>
-      <Container className='flex flex-col md:flex-row items-center'>
-        <Box className='flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
-          <Typography variant='h3' component='h1' className='mb-4 font-medium text-white'>
-            Hi, I'm Shelmi
-          </Typography>
-          <Typography variant='body1' component='p' className='mb-8 leading-relaxed text-gray-400'>
-            To achieve a challenging position in a professional organization through self-improvement by excelling in all responsibilities with sincere hard work, dedication & commitment. To work towards the development of the organization & grow with it.
-          </Typography>
-          <Box className='flex justify-center space-x-4'>
-            <Button href='#experience' variant='contained' color='primary'>
-              Experience
-            </Button>
-            <Button href='#projects' variant='contained' color='secondary'>
-              See My Own Projects
-            </Button>
-          </Box>
+    <Container>
+      <Paper>
+        <Grid container spacing={2}>
+          <Grid item md={6} xs={12}>
+        <Box className='flex flex-col items-center md:w-1/2 lg:pr-24 md:pr-16 mb16 md:mb-0 '>
+<Card style={{ padding: '20px', backgroundColor: '#F4ECE6',width:'500px' }}>
+<CardContent className='flex flex-col items-center'>
+<img src={Profile} alt='Profile' className='w-32 h-32 mb-4 rounded-full' />
+              <Typography variant='h4' component='h1' className='text-black font-bold underline-offset-4 mb-2'>
+             Shelmi K A
+              </Typography>
+              <Typography variant='h8' component='h3' className='text-black font-thin mb-2'>
+           React.Js Developer
+              </Typography>
+              <Box className='flex justify-center space-x-4 mb-4 pt-11'>
+                <IconButton href='https://www.linkedin.com' target='_blank' className='text-black'>
+                  <LinkedIn />
+                </IconButton>
+                <IconButton href='https://github.com' target='_blank' className='text-black'>
+                  <GitHub />
+                </IconButton>
+                <IconButton href='https://twitter.com' target='_blank' className='text-black'>
+                  <Twitter />
+                </IconButton>
+                </Box>
+</CardContent>
+</Card>
         </Box>
-        <Box className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'>
-          <img  className='rounded-full w-32 h-32 object-cover object-center'
-             
-             alt='hero' src={Profile} />
-        </Box>
-      </Container>
-    </section>
-  );
+          </Grid>
+          <Grid item md={6} xs={12}>
+            <Box className='flex-grow flex flex-col items-center md:items-start text-center md:text-left'>
+              <Typography variant='h2' component='h1' className='mb-4 font-medium text-gray-900'>
+                Hello
+              </Typography>
+              <Typography variant='h8'  className='mb-4 font-medium text-gray-900'>
+              Here's who I am & what I do
+              </Typography>
+              <Box className='flex justify-center md:justify-start space-x-4'>
+                  <Button href='#resume' variant='contained' color='primary'>
+                    Resume
+                  </Button>
+                  <Button href='#projects' variant='contained' color='secondary'>
+                    Projects
+                  </Button>
+                </Box>
+                <Typography variant='body1' component='p' className='mb-8 leading-relaxed text-gray-600'>
+                  To achieve a challenging position in a professional organization through self-improvement by excelling in all responsibilities with sincere hard work, dedication & commitment. To work towards the development of the organization & grow with it.
+                </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
+  )
 }
 
-export default About;
+export default About
