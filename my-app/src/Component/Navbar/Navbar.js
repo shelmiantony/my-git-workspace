7474c93b60413eec87c9f19f27616e5cb68859ca
Shelmi <shelmika23@gmail.com>
2024-07-14 16:42:43 +0530

    Added navbar

--- my-app/src/Component/Navbar/Navbar.js
+++ my-app/src/Component/Navbar/Navbar.js
@@ -1,8 +1,15 @@
+import { AppBar ,Toolbar,Typography} from '@mui/material'
 import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <AppBar position="static" className="bg-gray-900">
+    <Toolbar>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        My Portfolio
+      </Typography>
+      </Toolbar>
+      </AppBar>
   )
 }
 
